7ddca49d96ae12456435f4753adafa30948deaaa
codepundit1 <jahid35-266@diu.edu.bd>
2022-05-20 01:39:18 +0600

    content update

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     use HasFactory;
+    protected $table = 'booking';
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'checkin',
+        'checkout',
+    ];
 }
--- resources/views/resort.blade.php
+++ resources/views/resort.blade.php
@@ -4,18 +4,25 @@
 
  <div class="container">
     <div class="card-group">
+        <div class="row">
+            <div class="col-md-6">
+
+
         @foreach ($resorts as $resort)
         <div class="card">
-            <img src="" class="card-img-top" alt="...">
+            <img src="{{ asset('uploads/resorts/'. $resort->image) }}" width="100%" height="200px" alt="">
             <div class="card-body">
               <h5 class="card-title">{{ $resort->name }} </h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text">{{ $resort->description }}</p>
             </div>
             <div class="card-footer">
               <a class="btn btn-primary btn-sm" href="{{ url('booking') }}"> Book Now</a>
             </div>
           </div>
         @endforeach
+
+        </div>
+        </div>
       </div>
  </div>
 @endsection
